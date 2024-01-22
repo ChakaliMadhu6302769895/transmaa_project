2fbb6cf002e55392b184e71462ef8aca33271c75
ChakaliMadhu6302769895 <gauthamroyalz12324@gmail.com>
2024-01-22 13:52:21 +0530

    Initial commit

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool showLogo = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        showLogo = true;
+      });
+      Future.delayed(const Duration(seconds: 2), () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (!showLogo)
+              AnimatedLorry(),
+            if (showLogo)
+              Column(
+                children: [
+                  ClipRect(
+                    child: Image.asset('assets/Finalized_logo_TRANSMAA-removebg-preview.png'),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 50, // Adjust width as needed
+                        child: Divider(
+                          color: Colors.white,
+                          thickness: 2, // Adjust thickness as needed
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                      Container(
+                        width: 50, // Adjust width as needed
+                        child: Divider(
+                          color: Colors.white,
+                          thickness: 2, // Adjust thickness as needed
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Moving every load, all day, every day.',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedLorry extends StatefulWidget {
+  @override
+  _AnimatedLorryState createState() => _AnimatedLorryState();
+}
+
+class _AnimatedLorryState extends State<AnimatedLorry> {
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder(
+      tween: Tween(begin: -200.0, end: MediaQuery.of(context).size.width),
+      duration: const Duration(seconds: 3),
+      builder: (context, double value, child) {
+        return Transform.translate(
+          offset: Offset(value, 0),
+          child: Image.asset(
+            'assets/lorry-removebg-preview.png',
+            width: 400,
+            height: 400,
+          ),
+        );
+      },
+    );
+  }
+}
